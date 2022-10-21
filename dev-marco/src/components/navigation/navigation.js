29cf692b0386c1369523b3036d2e97a7197cd475
MarcoValeri <marcovaleri@hotmail.it>
2022-10-21 16:09:59 +0100

    Add responsive navigation

--- dev-marco/src/components/navigation/navigation.js
+++ dev-marco/src/components/navigation/navigation.js
@@ -4,12 +4,19 @@ import './navigation.scss';
 const Navigation = () => {
     return (
         <nav className="nav">
-            <ul className="nav__menu">
-                <li className="nav__menu-item"><a className="link-no-style body-3 link-effect-load link-highlight" href="#">Home</a></li>
-                <li className="nav__menu-item"><a className="link-no-style body-3 link-effect-load" href="#">Portfolio</a></li>
-                <li className="nav__menu-item"><a className="link-no-style body-3 link-effect-load" href="#">About Me</a></li>
-                <li className="nav__menu-item"><a className="link-no-style body-3 link-effect-load" href="#">Contact</a></li>
-            </ul>
+            <div className="nav__toggle">
+                <label for="nav menu" aria-label="nav menu"></label>
+                <input id="nav menu" name="nav menu" class="nav__input" type="checkbox" aria-label="nav menu" />
+                <span class="nav__span"></span>
+                <span class="nav__span"></span>
+                <span class="nav__span"></span>
+                <ul className="nav__menu">
+                    <li className="nav__menu-item"><a className="link-no-style body-3 link-effect-load link-highlight" href="#">Home</a></li>
+                    <li className="nav__menu-item"><a className="link-no-style body-3 link-effect-load" href="#">Portfolio</a></li>
+                    <li className="nav__menu-item"><a className="link-no-style body-3 link-effect-load" href="#">About Me</a></li>
+                    <li className="nav__menu-item"><a className="link-no-style body-3 link-effect-load" href="#">Contact</a></li>
+                </ul>
+            </div>
         </nav>
     )
 }
