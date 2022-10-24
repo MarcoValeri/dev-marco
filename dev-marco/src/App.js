929586fb3519756acf086beb753891e6ef87f0be
MarcoValeri <marcovaleri@hotmail.it>
2022-10-24 17:08:33 +0100

    Start infinite carousel

--- dev-marco/src/App.js
+++ dev-marco/src/App.js
@@ -1,10 +1,14 @@
 import Header from './components/header/header';
+import CarouselInfinite from './components/carousel-infinite/carousel-infinite';
 
 import './App.scss';
 
 function App() {
   return (
-    <Header />
+    <>
+        <Header />
+        <CarouselInfinite />
+    </>
   );
 }
 
---
+++ dev-marco/src/components/carousel-infinite/carousel-infinite.js
@@ -0,0 +1,49 @@
+// Import style file
+import './carousel-infinite.scss';
+
+const CarouselInfinite = () => {
+    return (
+        <section className="carousel-infinite">
+            <div className="carousel-infinite__container">
+                <div className="carousel-infinite__scroll-wrapper">
+                    <div className="carousel-infinite__item">
+                        <h1>item 1</h1>
+                    </div>
+                    <div className="carousel-infinite__item">
+                        <h1>item 1</h1>
+                    </div>
+                    <div className="carousel-infinite__item">
+                        <h1>item 1</h1>
+                    </div>
+                    <div className="carousel-infinite__item">
+                        <h1>item 1</h1>
+                    </div>
+                    <div className="carousel-infinite__item">
+                        <h1>item 1</h1>
+                    </div>
+                    <div className="carousel-infinite__item">
+                        <h1>item 1</h1>
+                    </div>
+                    <div className="carousel-infinite__item">
+                        <h1>item 1</h1>
+                    </div>
+                    <div className="carousel-infinite__item">
+                        <h1>item 1</h1>
+                    </div>
+                    <div className="carousel-infinite__item">
+                        <h1>item 1</h1>
+                    </div>
+                    <div className="carousel-infinite__item">
+                        <h1>item 1</h1>
+                    </div>
+                    <div className="carousel-infinite__item">
+                        <h1>item 1</h1>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+
+export default CarouselInfinite;
