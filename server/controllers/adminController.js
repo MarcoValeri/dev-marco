9afdeaa6593f67fbaba4c13b4510caacc1b2773f
MarcoValeri <marcovaleri@hotmail.it>
2022-11-03 16:10:02 +0000

    Add admin read json projects api

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -1,6 +1,43 @@
+const fs = require('fs');
+const { runInNewContext } = require('vm');
+
 exports.adminDashboard = (req, res, next) => {
-    // res.send(`<h1>Admin Section</h1>`);
-    res.render('admin', {
+
+    res.render('admin-dashboard', {
         pageTitle: 'Admin Dashboard'
     });
+
 };
+
+exports.adminReadProjectsApi = (req, res, next) => {
+
+    /**
+     * Read JSON API file
+     */
+     let projectsJsonData = fs.readFileSync('./api/projects.json');
+     let projectsData = JSON.parse(projectsJsonData);
+
+     // projectsData.projects.forEach(project => {
+     //     console.log(`Project ID: ${project.projectId}`);
+     // })
+
+     res.render('read-projects-api', {
+         pageTitle: 'Admin Projects API',
+         projectsApi: projectsData
+     });
+
+}
+
+exports.adminWriteProjectsApi = (req, res, next) => {
+
+    /**
+     * Write JSON API file
+     */
+    //  let newProjectJsonData = 'Hello Node';
+    //  fs.writeFileSync('./api/projects.json', JSON.stringify(newProjectJsonData));
+
+     res.render('admin-write-projects-api', {
+        pageTitle: 'Admin Projects API'
+     });
+
+}
--- server/routes/admin.js
+++ server/routes/admin.js
@@ -5,6 +5,8 @@ const adminController = require('../controllers/adminController');
 
 const router = express.Router();
 
-router.use('/admin', adminController.adminDashboard);
+router.use('/admin/write-projects-api', adminController.adminWriteProjectsApi);
+router.use('/admin/read-projects-api', adminController.adminDashboard);
+router.use('/admin/dashboard', adminController.adminDashboard);
 
 module.exports = router;
